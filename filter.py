c581bece44c0fa71b8c0d8055c8b5859bbec257b
yibin <yibin.net@qq.com>
2012-02-25 09:23:14 +0800

    delete

--- filter.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-from datetime import timedelta
-from google.appengine.ext import webapp
-register = webapp.template.create_template_register()
-@register.filter
-
-def timezone(value,offset):
-    return value + timedelta(hours=offset)
